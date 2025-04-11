9d6c966169537ab03801359eb2fb828f63c3640d
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-04-11 10:59:10 +0000

    caio# Please enter the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__) 
-
+lista=[1,2,3,4,5]
 
 @app.route("/")
 def index():
-    return "ciao"
+    return render_template("index.html", lista=lista)
 
+@app.route("/profilo")
+def profilo():
 
+    return "Questa è la pagina profilo"
 
 
 
